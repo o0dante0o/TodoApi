4feb956bc092a7e9c9315224870698bf5680a7e7
o0dante0o <j.a@a-vision.co>
2024-07-24 15:24:20 -0600

    2024-07-27 | First Commit

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<TodoItem> Todos { get; set; }
+    }
+}
---
+++ TodoApi/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string ?Name { get; set; }
+        public bool isComplete { get; set; }
+    }
+}
